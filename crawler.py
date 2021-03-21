b6cb2bf43840ae2e24f8969047da197b7d731007
Ezra Erives <ezraerives@gmail.com>
2021-03-20 23:15:57 -0400

    Can navigate to login

--- crawler.py
+++ crawler.py
@@ -4,4 +4,8 @@ import time
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get('http://www.mitrecsports.com/')
-time.sleep(100)
+
+driver.implicitly_wait(5)
+login_link = driver.find_element_by_id('menu-item-647').find_element_by_tag_name('a')
+print(login_link.get_attribute('text'))
+login_link.click()
